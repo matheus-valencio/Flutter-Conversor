a70ddf215ea8b8419b41ffde52f10dfc5f40a8a9
Matheus Valencio Fonseca <matheus.fonseca5@fatec.sp.gov.br>
2022-03-20 01:01:40 -0300

    Commit teste

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Conversor de Temperatura.',
+      title: 'Conversor de Temperatura',
       theme: ThemeData(
         primarySwatch: Colors.amber,
       ),
